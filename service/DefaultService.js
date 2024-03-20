1dd66f60e330ec7fb3c1a2dc848cb244d19e6c2a
m-gdz <martingrafik421@gmail.com>
2024-03-20 21:19:13 +0100

    listeners fix

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -34,58 +34,49 @@ exports.runIndexingPipeline = async function (body) {
 const { jobStatusEmitter } = require('./JobManager');
 const s = require('swagger-express-middleware');
 
+let clients = []; // This will hold active clients
+
+function broadcastEvent(eventType, data) {
+    console.log("broadcast");
+    clients.forEach(client => {
+        client.res.write(`event: ${eventType}\n`);
+        client.res.write(`data: ${JSON.stringify(data)}\n\n`);
+        client.res.write(`id: ${client.counter}\n\n`);
+    });
+}
+
+// Setup the shared listeners
+jobStatusEmitter.on('jobStatusChanged', jobStatus => {
+    broadcastEvent('jobStatusChanged', jobStatus);
+});
+
+jobStatusEmitter.on('cronJobStatusChanged', cronJobStatus => {
+    broadcastEvent('cronJobStatusChanged', cronJobStatus);
+});
+
+let counter = 0;
+
 exports.subscribeToEvents = async function (req, res) {
-
-  let counter = 0;
-
-  // Set headers for SSE
-  res.writeHead(200, {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-
-  res.write('event: connected\n');
-  const jobsStatus = await getAllJobsStatus();
-  const cronJobsStatus = await getAllCronJobsStatus();
-
-  // Constructing the response object
-  const response = {
-      jobs: jobsStatus,
-      cronJobs: cronJobsStatus
-  };
-
-  // Converting the response object to a JSON string
-  const jsonResponse = JSON.stringify(response);
-
-  // Logging the response
-  res.write(`data: ${jsonResponse}\n\n`);
-  res.write(`id: ${counter}\n\n`); 
-
-  
-  counter++;
-
-  
-  // Listen for job status updates and send them to the client
-  jobStatusEmitter.on('jobStatusChanged', (jobStatus) => {
-    console.log("fire event");
-    res.write('event: jobStatusChanged\n')
-    res.write(`data: ${JSON.stringify(jobStatus)}\n\n`);
-    res.write(`id: ${counter}\n\n`); 
+    // Set headers for SSE
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
     counter++;
-  });
+    const client = { req, res, counter};
+    clients.push(client);
 
-  jobStatusEmitter.on('cronJobStatusChanged', (jobStatus) => {
-    console.log("fire cron event");
-    res.write('event: cronJobStatusChanged\n')
-    res.write(`data: ${JSON.stringify(jobStatus)}\n\n`);
-    res.write(`id: ${counter}\n\n`); 
-    counter++;
-  });
+    // Send initial data to the client
+    const jobsStatus = await getAllJobsStatus();
+    const cronJobsStatus = await getAllCronJobsStatus();
+    const response = { jobs: jobsStatus, cronJobs: cronJobsStatus };
+    res.write(`data: ${JSON.stringify(response)}\n\n`);
+    res.write(`id: ${client.counter}\n\n`);
 
-  // Clean up when the client disconnects
-  req.on('close', () => {
-      jobStatusEmitter.removeAllListeners('jobStatusChanged');
-  });
+    // Clean up when the client disconnects
+    req.on('close', () => {
+        clients = clients.filter(c => c !== client);
+    });
 };
